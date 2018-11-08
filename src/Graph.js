7e4bd334763931be0608227444649b66e5ea3c62
maiyamag <maiyamag@yahoo-corp.jp>
2018-11-08 22:00:52 +0900

    modify

--- src/Graph.js
+++ src/Graph.js
@@ -1,8 +1,62 @@
 import React from 'react'
+import { ResponsiveLine } from '@nivo/line'
+import graphData from './graphData'
 
 const Graph = () => (
   <div>
-    Hello
+    Hello On Graph
+    <ResponsiveLine 
+      data={graphData}
+      margin={{
+        "top": 50,
+        "right": 110,
+        "bottom": 50,
+        "left": 60
+      }}
+      xScale={{
+        "type": "linear"
+      }}
+      yScale={{
+        "type": "linear",
+        "stacked": true,
+        "min": "auto",
+        "max": "auto"
+      }}
+      minY="auto"
+      maxY="auto"
+      stacked={true}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+          "orient": "bottom",
+          "tickSize": 5,
+          "tickPadding": 5,
+          "tickRotation": 0,
+          "legend": "transportation",
+          "legendOffset": 36,
+          "legendPosition": "middle"
+      }}
+      axisLeft={{
+          "orient": "left",
+          "tickSize": 5,
+          "tickPadding": 5,
+          "tickRotation": 0,
+          "legend": "count",
+          "legendOffset": -40,
+          "legendPosition": "middle"
+      }}
+      dotSize={10}
+      dotColor="inherit:darker(0.3)"
+      dotBorderWidth={2}
+      dotBorderColor="#ffffff"
+      enableDotLabel={true}
+      dotLabel="y"
+      dotLabelYOffset={-12}
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+    />
+    Hello Below Graph
   </div>
 )
 
---
+++ src/graphData.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 'id_a',
+    color: 'red',
+    data: [
+      {
+        x: 1,
+        y: 3,
+      },
+      {
+        x: 2,
+        y: 2,
+      },
+      {
+        x: 3,
+        y: 5,
+      },
+    ]
+  },
+  {
+    id: 'id_b',
+    color: 'blue',
+    data: [
+      {
+        x: 1,
+        y: 2,
+      },
+      {
+        x: 2,
+        y: 6,
+      },
+      {
+        x: 3,
+        y: 3,
+      },
+    ]
+  }
+]
